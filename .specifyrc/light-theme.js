007b0bb2fdc6bc1480b9b493205e3301239724ce
UlysseGebe <Uly35534e31>
2020-11-10 19:53:07 +0100

    Add sass + Add Specify

---
+++ .specifyrc/light-theme.js
@@ -0,0 +1,29 @@
+module.exports = {
+    repository: "@hetic/G02-E05-Light",
+    personalAccessToken: "8d6b6515fa3313d68d20f4263f7985d0ce83ffe17b07b114e48b3bd6eb535dff",
+    rules: [
+      {
+        name: 'Colors / Light Theme',
+        path: "./src/assets/style/colors/light.scss",
+        filter: {
+          types: ["color"]
+        },
+        parsers: [
+          {
+            name: "kebabcasify",
+            options: {
+              keys: ["name"]
+            }
+          },
+          {
+            name: "to-css-custom-properties",
+            options: {
+                formatConfig: {
+                  selector: ":root[data-theme='light']",
+                },
+            },
+          },
+        ],
+    }
+  ]
+}
